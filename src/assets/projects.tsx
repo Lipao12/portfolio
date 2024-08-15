3bb702e9f9b20c17c4cf254a303c8a0ee808ef39
Filipe Mai <f.nunesmai19@gmail.com>
2024-08-15 15:55:45 -0300

    .

--- src/assets/projects.tsx
+++ src/assets/projects.tsx
@@ -1,4 +1,3 @@
-import projectImage1 from "./project-1.png";
 import projectImage2 from "./project-2.png";
 import projectImage3 from "./project-3.png";
 import projectImage4 from "./project-4.png";
